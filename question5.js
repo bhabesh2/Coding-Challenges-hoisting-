bc2e9fd450d230c6566a89680e4da8253e73901e
bhabesh2 <jsarma058@gmail.com>
2023-04-03 23:19:05 +0530

    example Function

---
+++ question5.js
@@ -0,0 +1,7 @@
+function exampleFunction() {
+    console.log(a); // ReferenceError: Cannot access 'a' before initialization
+    let a = "I am using let";
+  }
+  
+  exampleFunction();
+  
--- tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-console.log(num2);// reference error can't access;
-    // console.log(mun3);// r
